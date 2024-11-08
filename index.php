10201cc3f805dc207a85da3d8081cf12a1747d42
JC0D <128965653+JC0D@users.noreply.github.com>
2024-11-09 05:43:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
-
+require_once 'Person.php';
 require_once 'Employee.php';
 require_once 'Main.php';
+require_once 'CommissionEmployee.php';
+require_once 'HourlyEmployee.php';
+require_once 'PieceWorker.php';
+require_once 'Roster.php';
 
 $menu = new Main();
-$menu->display();
+$menu->startMenu();
